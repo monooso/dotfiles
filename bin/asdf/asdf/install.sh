89c1c3e82a9769c33e4bf1e7bc1053afe93c7b93
Stephen Lewis <gpg@stephenlewis.me>
2023-03-15 17:43:52 +0000

    Improve bootstrap script, move ZSH key bindings to separate file
    bindkey -v throws an error when bootstrapping (it's fine otherwise).
    
    Hoping this fixes it.

--- bin/asdf/asdf/install.sh
+++ bin/asdf/asdf/install.sh
@@ -3,6 +3,7 @@ if command -v brew &> /dev/null
 then
   brew install coreutils curl git
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.2
+  source "$HOME/.asdf/asdf.sh"
 else
   echo "Unable to install asdf. Check that Homebrew is installed."
   exit 1
---
+++ bin/bootstrap.sh
@@ -0,0 +1,58 @@
+info() {
+  echo "ℹ️ $1"
+}
+
+success() {
+  echo "✅ $1"
+}
+
+# Install Homebrew
+info "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+success "Homebrew installed"
+
+# Install Homebrew packages
+info "Installing Homebrew packages..."
+
+brew update && brew install \
+  bat \
+  curl \
+  fd \
+  fzf \
+  git \
+  lazygit \
+  neovim \
+  ripgrep \
+  stow \
+  tmux \
+  tree \
+  wget
+
+success "Homebrew packages installed"
+
+# Install asdf
+info "Installing asdf..."
+source bin/asdf/asdf/install.sh
+success "asdf installed"
+
+# Install asdf Node.js plugin
+info "Installing asdf Node.js plugin..."
+source bin/asdf/nodejs/install.sh
+success "asdf Node.js plugin installed"
+
+# Install Node.js LTS for use globally
+info "Installing Node.js LTS..."
+asdf plugin-update nodejs
+asdf install nodejs lts
+success "Node.js LTS installed"
+
+# Stow our dotfiles
+info "Installing dotfiles..."
+stow -t ~/ .
+success "Dotfiles installed"
+
+# Source the .zshrc
+info "Sourcing .zshrc"
+source ~/.zshrc
+success "All done"
