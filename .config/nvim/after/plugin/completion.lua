5a042a2077e97e022a19496ad700f4ab44f5cc58
Stephen Lewis <gpg@stephenlewis.me>
2023-07-29 15:44:06 +0100

    Fix issue with overlapping signature floats

--- .config/nvim/after/plugin/completion.lua
+++ .config/nvim/after/plugin/completion.lua
@@ -1,10 +1,8 @@
 local cmp = require('cmp')
-local lspkind = require('lspkind')
 
 if cmp ~= nil then
   cmp.setup({
     completion = { completeopt = 'menu,menuone,noinsert' },
-    formatting = { format = lspkind.cmp_format() },
     mapping = cmp.mapping.preset.insert({
       ['<CR>'] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Insert,
@@ -27,7 +25,7 @@ if cmp ~= nil then
       { name = 'luasnip', keyword_length = 1, max_item_count = 4 },
       { name = 'nvim_lsp', keyword_length = 1, max_item_count = 4 },
       { name = 'nvim_lua', keyword_length = 1, max_item_count = 4 },
-      { name = 'copilot', keyword_length = 1, max_item_count = 3 },
+      { name = 'copilot', keyword_length = 3, max_item_count = 3 },
     })
   })
 end
