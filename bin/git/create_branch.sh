a8bd5180d82d519a9b990151cfe3291f79dac8b7
Stephen Lewis <gpg@stephenlewis.me>
2023-09-13 21:47:56 +0100

    Refactor git create branch command; change alias to gbc

---
+++ bin/git/create_branch.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Script: create_branch.sh
+#
+# Description: Helper script to prevent me from accidentally branching off anything other than the main git branch.
+# 
+# Usage: bash create_branch.sh new_branch_name
+
+# Load helper functions.
+source "$(dirname "$0")/utils.sh"
+
+current_branch="$(git_current_branch)"
+
+if [ "$current_branch" != "main" ] && [ "$current_branch" != "master" ]; then
+  echo "WOAH THERE, COWBOY"
+  read -p "Are you sure you want to branch off $current_branch (y/n): " proceed
+
+  case $proceed in
+    [Yy])
+      git_create_branch $1;;
+
+    *)
+      echo "Crisis averted";;
+  esac
+else
+  git_create_branch $1
+fi
