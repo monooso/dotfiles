1cfa9d82fc4843d2ac79540bd73dc12e5d8a3e7c
Stephen Lewis <gpg@stephenlewis.me>
2023-09-23 08:13:18 +0100

    Switch to lazy.nvim for plugin management
    Packer is no longer supported.

--- .config/nvim/after/plugin/completion.lua
+++ .config/nvim/after/plugin/completion.lua
@@ -1,10 +1,14 @@
-local cmp = require('cmp')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 if cmp ~= nil then
   cmp.setup({
-    completion = { completeopt = 'menu,menuone,noinsert' },
+    completion = { completeopt = "menu,menuone,noinsert" },
+    formatting = {
+      format = lspkind.cmp_format({ mode = "symbol_text" }),
+    },
     mapping = cmp.mapping.preset.insert({
-      ['<CR>'] = cmp.mapping.confirm({
+      ["<CR>"] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Insert,
         select = true
       })
@@ -12,20 +16,25 @@ if cmp ~= nil then
     preselect = cmp.PreselectMode.Item,
     snippet = {
       expand = function(args)
-        require('luasnip').lsp_expand(args.body)
+        require("luasnip").lsp_expand(args.body)
       end
     },
     sorting = {
       comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
         cmp.config.compare.order,
       }
     },
     sources = cmp.config.sources({
-      { name = 'path' },
-      { name = 'copilot', keyword_length = 2, max_item_count = 2 },
-      { name = 'luasnip', keyword_length = 1, max_item_count = 4 },
-      { name = 'nvim_lsp', keyword_length = 1 },
-      { name = 'nvim_lua', keyword_length = 1 },
+      { name = "path" },
+      { name = "luasnip", keyword_length = 1 },
+      { name = "nvim_lsp", keyword_length = 1 },
+      { name = "nvim_lua", keyword_length = 1 },
     })
   })
 end
---
+++ .config/nvim/lua/plugins/navigation.lua
@@ -0,0 +1,5 @@
+return {
+  "lambdalisue/fern.vim",                                                           -- A better netrw.
+  { "ThePrimeagen/harpoon", dependencies = { "nvim-lua/plenary.nvim" } },           -- File jump list.
+  { "nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' } },  -- CMD-P on steroids.
+}
