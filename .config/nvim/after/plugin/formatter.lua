fa65dd5d8a3a5f8314a6b5a87c7da3ed0ee34124
Stephen Lewis <gpg@stephenlewis.me>
2023-12-07 16:12:56 +0000

    Add Biome to the list of (Java|Type)Script formatters

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -2,8 +2,8 @@ require("conform").setup({
   formatters_by_ft = {
     elixir = { "mix format" },
     lua = { "stylua" },
-    javascript = { { "deno fmt", "prettierd", "prettier" } },
-    typescript = { { "deno fmt", "prettierd", "prettier" } },
+    javascript = { { "biome", "deno fmt", "prettierd", "prettier" } },
+    typescript = { { "biome", "deno fmt", "prettierd", "prettier" } },
   },
   format_on_save = {
     lsp_fallback = true,
