a433815f6f6db9f3ed925d87024b39b91c4cf10b
Stephen Lewis <gpg@stephenlewis.me>
2023-12-10 21:38:04 +0000

    Replace calls to deprecated vim.loop with vim.uv

--- .config/nvim/lua/monooso/lsp.lua
+++ .config/nvim/lua/monooso/lsp.lua
@@ -76,7 +76,7 @@ M.setup = function()
     on_init = function(client)
       local path = client.workspace_folders[1].name
 
-      if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      if not vim.uv.fs_stat(path .. '/.luarc.json') and not vim.uv.fs_stat(path .. '/.luarc.jsonc') then
         client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
           Lua = {
             runtime = { version = 'LuaJIT' },
