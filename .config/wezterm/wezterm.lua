1ee20719dda2ed7ca9b65bb4c42e0bd17c8a8ec9
Stephen Lewis <gpg@stephenlewis.me>
2024-07-06 18:58:26 +0100

    Maximise Wezterm window on open
    Update the Wezterm config file to use the new (-ish)
    `wezterm.config_builder()` function.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,17 @@
--- Reasonable colour schemes, I think. I'm still not sure how to get Neovim
--- to inherit the terminal colour scheme. Pretty sure it has *something* to do
--- with the "truecolors" and `vim.o.termguicolors`.
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+-- Always maximise the window when the GUI is attached.
+wezterm.on('gui-attached', function(_)
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
+end)
+
+-- Define some "known good" colour schemes, so I'm not faffing about every time I fancy a change.
 local color_schemes = {
   dark = {
     black_metal = "Black Metal (Bathory) (base16)",
@@ -15,21 +26,26 @@ local color_schemes = {
   }
 }
 
+-- Configure Wezterm.
+local config = wezterm.config_builder()
 
-return {
-  check_for_updates = false,
-  color_scheme = color_schemes.dark.fahrenheit,
-  default_cursor_style = 'BlinkingBar',
+config.check_for_updates = false
+config.color_scheme = color_schemes.dark.fahrenheit
+config.default_cursor_style = 'BlinkingBar'
 
-  -- Font. Wezterm bundles and uses JetBrains Mono by default.
-  -- @see https://wezfurlong.org/wezterm/config/fonts.html
-  font_size = 15,
-  line_height = 1.75,
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- Disable ligatures.
+-- Use Fish shell, always. Act as though it's the login shell.
+config.default_prog = { '/opt/homebrew/bin/fish', '--login' }
 
-  -- Chrome.
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
-  window_decorations = 'RESIZE'
-}
+-- Font. Wezterm bundles and uses JetBrains Mono by default.
+-- @see https://wezfurlong.org/wezterm/config/fonts.html
+config.font_size = 15
+config.line_height = 1.75
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable ligatures.
+
+-- Chrome.
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = 'RESIZE'
+
+return config
