f4ae76b0b28e9b2a2555fb9fa7a91e09e9136152
Stephen Lewis <gpg@stephenlewis.me>
2024-07-06 18:11:31 +0100

    Switch from asdf to mise

--- bin/asdf/nodejs/install.sh
+++
@@ -1,9 +0,0 @@
-# Only run if asdf and Homebrew are installed
-if [ -f $HOME/.asdf/asdf.sh ] && command -v brew &> /dev/null
-then
-  brew install gawk gpg
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-else
-  echo "Unable to install Node.js. Check that both asdf and Homebrew are installed."
-  exit 1
-fi
