8e93c128c42d279692284e647d848dd5f452da47
Stephen Lewis <gpg@stephenlewis.me>
2024-07-22 13:26:18 +0100

    Install LazyVim; remove most obvious annoyances
    There are plenty left.

--- .config/nvim/after/ftplugin/php.lua
+++
@@ -1,7 +0,0 @@
--- local buffer options
-local o = vim.bo
-
-o.expandtab = true
-o.shiftwidth = 4
-o.softtabstop = 4
-o.tabstop = 4
--- .config/nvim/after/indent/php.lua
+++
@@ -1,8 +0,0 @@
--- *setlocal* buffer options
-local o = vim.bo
-
--- Fix broken autoindentation
--- @see https://github.com/nvim-treesitter/nvim-treesitter/issues/462#issuecomment-700278736
-o.indentexpr = ''
-o.smartindent = true
-o.syntax = 'on'
