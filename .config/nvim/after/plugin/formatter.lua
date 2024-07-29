4254a1d39b3fcf829106d8ed05cd10548ca52dcc
Stephen Lewis <gpg@stephenlewis.me>
2024-07-29 17:28:20 +0100

    Fix conform.nvim deprecation errors

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -2,8 +2,8 @@ require("conform").setup({
   formatters_by_ft = {
     elixir = { "mix format" },
     lua = { "stylua" },
-    javascript = { { "biome", "deno fmt", "prettierd", "prettier" } },
-    typescript = { { "biome", "deno fmt", "prettierd", "prettier" } },
+    javascript = { "deno fmt", "prettierd", "prettier", stop_after_first = true },
+    typescript = { "deno fmt", "prettierd", "prettier", stop_after_first = true },
   },
   format_on_save = {
     lsp_fallback = true,
