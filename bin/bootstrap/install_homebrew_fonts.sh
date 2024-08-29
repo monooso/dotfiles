9e01b1063ec27223538d66dcfa3123dff714e53b
Stephen Lewis <gpg@stephenlewis.me>
2024-08-29 15:40:33 +0100

    Remove macOS-specific bootstrap scripts; update README

--- bin/bootstrap/install_homebrew_fonts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Helper script to install Homebrew packages.
-# This will be obsolete once we move to Homebrew Bundle.
-
-brew update && brew install \
-  font-iosevka-ss15 \
-  font-symbols-only-nerd-font
