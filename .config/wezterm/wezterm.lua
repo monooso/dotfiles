95644ada2f987bb035fd88abaa9c48a56ea180c3
Stephen Lewis <gpg@stephenlewis.me>
2024-09-10 09:29:29 +0100

    Improve appearance of window tabs in Wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -32,7 +32,6 @@ local color_schemes = {
 local config = wezterm.config_builder()
 
 config.check_for_updates = false
-config.color_scheme = color_schemes.light.tokyo
 config.default_cursor_style = 'BlinkingBar'
 
 -- Use Fish shell, always. Act as though it's the login shell.
@@ -44,10 +43,35 @@ config.font_size = 15
 config.line_height = 1.75
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable ligatures.
 
+-- Colours.
+config.color_scheme = color_schemes.light.tokyo
+
+config.window_frame = {
+  font = wezterm.font { family = 'JetBrains Mono' },
+  font_size = 13,
+  active_titlebar_bg = '#d2d4de'
+}
+
+config.colors = {
+  tab_bar = {
+    background = '#d2d4de',
+    active_tab = {
+      bg_color = '#e1e2e7',
+      fg_color = '#1f2335',
+    },
+    inactive_tab = {
+      bg_color = '#d2d4de',
+      fg_color = '#737aa2',
+    }
+  }
+}
+
 -- Chrome.
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
+-- config.tab_bar_at_bottom = true
+-- config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.show_tab_index_in_tab_bar = false
 config.window_decorations = 'RESIZE'
 
 return config
