e9dfc576f3d7d5e3dc92f4072439d54f3d53b988
Stephen Lewis <gpg@stephenlewis.me>
2024-10-16 10:34:18 +0100

    Fix Laravel Sail check

--- .config/nvim/after/ftplugin/php.lua
+++ .config/nvim/after/ftplugin/php.lua
@@ -6,8 +6,8 @@ o.shiftwidth = 4
 o.softtabstop = 4
 o.tabstop = 4
 
--- Use the Sail PHP executable, if it exists
-if vim.fn.executable('sail') then
+-- We use Sail on Linux.
+if vim.uv.os_uname().sysname == 'Linux' then
     vim.g['test#php#pest#executable'] = 'sail artisan test'
     vim.g['test#php#phpunit#executable'] = 'sail artisan test'
 end
