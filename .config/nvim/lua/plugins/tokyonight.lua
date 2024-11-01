f49ba96f21215a497d57ee688914b706c3fa9c87
Stephen Lewis <gpg@stephenlewis.me>
2024-11-01 13:38:18 +0000

    Ensure split lines are visible in TokyoNight theme

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,5 +1,10 @@
 return {
     'folke/tokyonight.nvim',
+    config = {
+        on_colors = function(c)
+            c.border = c.blue0
+        end
+    },
     lazy = false,
     priority = 1000
 }
