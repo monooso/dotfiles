45ac5d575f4438a3fcad45405a7756229def6011
Stephen Lewis <gpg@stephenlewis.me>
2024-11-01 13:57:42 +0000

    Reinstate nvim-cmp source plugins

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,8 +1,10 @@
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
-    'onsails/lspkind.nvim',
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-path',
+    'onsails/lspkind.nvim',
     'saadparwaiz1/cmp_luasnip'
   }
 }
