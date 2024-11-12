2fa2e835d184d7cca724c7003922481fa6dce58b
Stephen Lewis <gpg@stephenlewis.me>
2024-11-12 12:45:52 +0000

    Format Vue files with Prettier, increase formatter timeout
    Because Prettier is slow as molasses.

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -1,6 +1,9 @@
 require('conform').setup({
   format_on_save = {
     lsp_format = 'fallback',
-    timeout_ms = 500
+    timeout_ms = 1500
+  },
+  formatters_by_ft = {
+    vue = { 'prettierd', 'prettier', stop_after_first = true }
   }
 })
