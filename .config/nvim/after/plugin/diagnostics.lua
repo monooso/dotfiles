9ede4d800ff485b35c9396ce6d7e40391d26c00b
Stephen Lewis <code@stephenlewis.me>
2025-04-11 09:54:40 +0100

    Update deprecated function calls

--- .config/nvim/after/plugin/diagnostics.lua
+++ .config/nvim/after/plugin/diagnostics.lua
@@ -11,8 +11,8 @@ vim.diagnostic.config({
 wk.add({
   mode = { 'n' },
   { '<leader>d',  group = 'Diagnostics' },
-  { '<leader>dl', function() vim.diagnostic.open_float({ scope = 'buffer' }) end, desc = 'List buffer diagnostic messages' },
-  { '<leader>dn', vim.diagnostic.goto_next,                                       desc = 'Next diagnostic message' },
-  { '<leader>dp', vim.diagnostic.goto_prev,                                       desc = 'Previous diagnostic message' },
-  { '<leader>dq', vim.diagnostic.setloclist,                                      desc = 'Send diagnostics to location list' },
+  { '<leader>dl', function() vim.diagnostic.open_float({ scope = 'buffer' }) end,   desc = 'List buffer diagnostic messages' },
+  { '<leader>dn', function() vim.diagnostic.jump({ count = 1, float = true }) end,  desc = 'Next diagnostic message' },
+  { '<leader>dp', function() vim.diagnostic.jump({ count = -1, float = true }) end, desc = 'Previous diagnostic message' },
+  { '<leader>dq', vim.diagnostic.setloclist,                                        desc = 'Send diagnostics to location list' },
 })
