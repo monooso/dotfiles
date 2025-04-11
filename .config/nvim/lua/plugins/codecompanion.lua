5a40317a4d594d4f217641bcca30e2d94154fb45
Stephen Lewis <code@stephenlewis.me>
2025-04-11 09:57:36 +0100

    Remove (apparently) unneeded plugin dependencies
    The CodeCompanion installation docs no longer list these plugins as
    required.

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -3,10 +3,6 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-treesitter/nvim-treesitter',
-        'hrsh7th/nvim-cmp',
-        'nvim-telescope/telescope.nvim',
-        { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } },
-        { 'stevearc/dressing.nvim',                    opts = {} },
     },
     config = true
 }
