677e5753f4b038801fbd3278e9e3e7014211bc2a
Stephen Lewis <code@stephenlewis.me>
2025-04-11 09:55:27 +0100

    Ditch (hopefully) redundant comment string plugin

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -1,17 +1,17 @@
 -- Context-aware commenting configuration.
 -- @see https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#native-commenting-in-neovim-010
-require('ts_context_commentstring').setup {
-    enable_autocmd = false,
-}
-
--- Tell Neovim to use the `commentstring` from Treesitter.
-local original_get_option = vim.filetype.get_option
-
-vim.filetype.get_option = function(filetype, option)
-    return option == 'commentstring'
-        and require('ts_context_commentstring.internal').calculate_commentstring()
-        or original_get_option(filetype, option)
-end
+-- require('ts_context_commentstring').setup {
+--     enable_autocmd = false,
+-- }
+--
+-- -- Tell Neovim to use the `commentstring` from Treesitter.
+-- local original_get_option = vim.filetype.get_option
+--
+-- vim.filetype.get_option = function(filetype, option)
+--     return option == 'commentstring'
+--         and require('ts_context_commentstring.internal').calculate_commentstring()
+--         or original_get_option(filetype, option)
+-- end
 
 -- General TreeSitter configuration.
 require('nvim-treesitter.configs').setup({
