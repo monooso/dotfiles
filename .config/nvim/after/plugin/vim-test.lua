f6b1e62e08c5e02de604309c53f8ba6a5bcf27cd
Stephen Lewis <code@stephenlewis.me>
2025-04-17 17:39:33 +0100

    Cobble together Sail test runner
    Detects whether the Sail executable is available, and modifies the
    `test#php#pest#executable` command accordingly. Not perfect, but it
    seems to work okay.

--- .config/nvim/after/plugin/vim-test.lua
+++ .config/nvim/after/plugin/vim-test.lua
@@ -11,3 +11,27 @@ wk.add({
     { '<leader>tv', '<cmd>TestVisit<cr>',   desc = 'Visit last test' },
   }
 })
+
+-- Attempt to configure vim-test using Lua. Possibly an exercise in futility.
+local M = {
+  env = { sail_executable = 'vendor/bin/sail' },
+  flags = { sail_is_enabled = false }
+}
+
+local function sail_is_enabled()
+  if M.flags.sail_is_enabled then
+    return true
+  end
+
+  M.flags.sail_is_enabled =
+      (vim.fn.filereadable(M.env.sail_executable) == 1)
+      and (vim.fn.filereadable('docker-compose.yml') == 1)
+
+  return M.flags.sail_is_enabled
+end
+
+-- This only works when opening Neovim in the project root,
+-- but that's typical.
+if sail_is_enabled() then
+  vim.g['test#php#pest#executable'] = 'sail pest'
+end
