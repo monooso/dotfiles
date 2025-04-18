1615d704f1d035e9fcaa8d8c2e75680a8ac6f105
Stephen Lewis <code@stephenlewis.me>
2025-04-18 21:23:42 +0100

    Use mini.completion instead of native Neovim completion
    I can't figure out how to make the built-in completion work with
    snippets, and life is too short.

--- .config/nvim/lua/monooso/lsp.lua
+++ .config/nvim/lua/monooso/lsp.lua
@@ -10,20 +10,7 @@ vim.lsp.enable({
     'elixirls',
     'html',
     'intelephense',
+    'lua_ls',
     'jsonls',
     'ts_ls',
-})
-
--- Enable auto-completion.
-local function lsp_on_attach_callback(event)
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-    if client:supports_method('textDocument/completion') then
-        vim.lsp.completion.enable(true, client.id, event.buffer, { autotrigger = true })
-    end
-end
-
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = lsp_on_attach_callback,
-    group = vim.api.nvim_create_augroup('monooso-lsp-attach', { clear = true })
 })
