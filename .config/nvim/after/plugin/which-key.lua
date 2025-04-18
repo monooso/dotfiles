7f50d148bd146b125d0cb87d4d5e7126593b78f1
Stephen Lewis <code@stephenlewis.me>
2025-04-18 21:19:59 +0100

    Remove Which Key, in favour of mini.clue

--- .config/nvim/after/plugin/which-key.lua
+++
@@ -1,8 +0,0 @@
--- Configure the Which Key plugin.
---
--- @see https://github.com/folke/which-key.nvim
-
-require('which-key').setup({
-  -- Wait 500ms before showing Which Key.
-  delay = 500,
-})
