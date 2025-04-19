887c60f3597e667e64cd68f3d4bc00f4709b1da9
Stephen Lewis <code@stephenlewis.me>
2025-04-19 13:23:38 +0100

    Change [g]oto key bindings to start with <Leader>n
    I foresee no problems.

--- .config/nvim/after/plugin/mini-pick.lua
+++ .config/nvim/after/plugin/mini-pick.lua
@@ -8,14 +8,17 @@ require('mini.extra').setup()
 local builtin = MiniPick.builtin
 local pickers = MiniExtra.pickers
 
-vim.keymap.set({ 'n' }, '<Leader>gd', pickers.diagnostic, { desc = 'Diagnostics' })
-vim.keymap.set({ 'n' }, '<Leader>gf', builtin.files, { desc = 'Files' })
-vim.keymap.set({ 'n' }, '<Leader>gg', builtin.grep_live, { desc = 'Grep' })
-vim.keymap.set({ 'n' }, '<Leader>gk', pickers.keymaps, { desc = 'Keymaps' })
-vim.keymap.set({ 'n' }, '<Leader>gm', pickers.marks, { desc = 'Marks' })
+-- The prefix `n` has no semantic meaning (it used to be `g`, for "go"). It's purely
+-- an exercise in efficiency. `<Leader>` (space) is under my left thumb, `n` is under
+-- my right index finger, and the modifiers are all under strong fingers on my left
+-- hand. So any command is left-right-left.
+vim.keymap.set({ 'n' }, '<Leader>nd', pickers.diagnostic, { desc = 'Diagnostics' })
+vim.keymap.set({ 'n' }, '<Leader>np', builtin.files, { desc = 'Files' })
+vim.keymap.set({ 'n' }, '<Leader>ng', builtin.grep_live, { desc = 'Grep' })
+vim.keymap.set({ 'n' }, '<Leader>nb', pickers.marks, { desc = 'Bookmarks' })
 
 -- The Neovim LSP implementation has this built-in (`gO`), but it opens the
 -- results in a split. This is much nicer.
-vim.keymap.set({ 'n' }, '<Leader>gs', function()
+vim.keymap.set({ 'n' }, '<Leader>ns', function()
   pickers.lsp({ scope = 'document_symbol' })
 end, { desc = 'Document symbols' })
