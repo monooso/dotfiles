534b5df662fc3684f3c50360ac5684b197a363f0
Stephen Lewis <code@stephenlewis.me>
2025-04-19 13:24:02 +0100

    Disable italics in Rose Pine theme

--- .config/nvim/lua/monooso/theme.lua
+++ .config/nvim/lua/monooso/theme.lua
@@ -2,7 +2,8 @@
 require('rose-pine').setup({
     variant = 'auto',
     dark_variant = 'main',
-    enable = { legacy_highlights = false }
+    enable = { legacy_highlights = false },
+    styles = { italic = false }
 })
 
-vim.cmd({ 'colorscheme', 'rose-pine' })
+vim.cmd('colorscheme rose-pine')
