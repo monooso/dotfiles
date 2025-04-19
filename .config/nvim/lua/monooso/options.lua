37f0c174108259be9b8a050ea3f5488891b03b96
Stephen Lewis <code@stephenlewis.me>
2025-04-19 01:38:25 +0100

    Remove unused local variable
    Apparently the Lua formatter also hates me.

--- .config/nvim/lua/monooso/options.lua
+++ .config/nvim/lua/monooso/options.lua
@@ -1,47 +1,45 @@
 -- Configures Neovim options.
-
-local g = vim.g
 local o = vim.opt
 
-o.mouse = 'a'                   -- Enable mouse mode, because TJ said it was okay.
+o.mouse = 'a'              -- Enable mouse mode, because TJ said it was okay.
 
-o.cursorline = true             -- Highlight the current line...
-o.cursorlineopt = 'number'      -- ...but just the line number.
+o.cursorline = true        -- Highlight the current line...
+o.cursorlineopt = 'number' -- ...but just the line number.
 
-o.number = true                 -- Show line numbers.
-o.relativenumber = true         -- Make them relative.
-o.signcolumn = 'yes:2'          -- Show the sign column. Allow two cells for diagnostics.
+o.number = true            -- Show line numbers.
+o.relativenumber = true    -- Make them relative.
+o.signcolumn = 'yes:2'     -- Show the sign column. Allow two cells for diagnostics.
 
-o.expandtab = true              -- Tabs are the devil's plaything.
-o.shiftwidth = 2                -- Set a sensible default indentation...
-o.softtabstop = 2               -- ...and again...
-o.tabstop = 2                   -- ...and once more.
+o.expandtab = true         -- Tabs are the devil's plaything.
+o.shiftwidth = 2           -- Set a sensible default indentation...
+o.softtabstop = 2          -- ...and again...
+o.tabstop = 2              -- ...and once more.
 
 -- o.wrap = false               -- Only savages wrap lines.
-o.breakindent = true            -- Indent wrapped lines.
-o.smartindent = true            -- Be smart about indenting.
+o.breakindent = true -- Indent wrapped lines.
+o.smartindent = true -- Be smart about indenting.
 
-o.undofile = true               -- Save undo history.
+o.undofile = true    -- Save undo history.
 
-o.ignorecase = true             -- Use case-insensitive searching...
-o.smartcase = true              -- ...unless we use `\C` or the search term contains capitals.
+o.ignorecase = true  -- Use case-insensitive searching...
+o.smartcase = true   -- ...unless we use `\C` or the search term contains capitals.
 
-o.list = true                   -- Show invisible characters.
+o.list = true        -- Show invisible characters.
 o.listchars = 'tab:▸ ,trail:•'
 
-o.foldenable = false            -- Disable folding; I never use it.
-o.visualbell = true             -- Stop beeping me, dammit.
-o.timeoutlen = 300              -- Don't wait around for keymappings to complete.
+o.foldenable = false -- Disable folding; I never use it.
+o.visualbell = true  -- Stop beeping me, dammit.
+o.timeoutlen = 300   -- Don't wait around for keymappings to complete.
 
-o.splitbelow = true             -- Open new splits below the current split.
-o.splitright = true             -- Open new splits to the right of the current split.
+o.splitbelow = true  -- Open new splits below the current split.
+o.splitright = true  -- Open new splits to the right of the current split.
 
-o.laststatus = 3                -- Use the global status line.
-o.cmdheight = 1                 -- Keep the command line small.
-o.shortmess = 'I'               -- Don't show the intro message on startup.
-o.showmode = false              -- Don't show the mode; it's already in the status line.
-o.scrolloff = 10                -- Minimum number of lines to show above and below cursor.
-o.confirm = true                -- Ask if I want to save unsaved changes on `:q`.
+o.laststatus = 3     -- Use the global status line.
+o.cmdheight = 1      -- Keep the command line small.
+o.shortmess = 'I'    -- Don't show the intro message on startup.
+o.showmode = false   -- Don't show the mode; it's already in the status line.
+o.scrolloff = 10     -- Minimum number of lines to show above and below cursor.
+o.confirm = true     -- Ask if I want to save unsaved changes on `:q`.
 
 -- Configure the built-in auto-complete menu.
 -- - Always show the menu, even if there is only one item.
@@ -62,6 +60,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight the yanked (copied) text',
   group = vim.api.nvim_create_augroup('monooso-highlight-yank', { clear = true }),
   callback = function()
-      vim.highlight.on_yank()
+    vim.highlight.on_yank()
   end,
 })
