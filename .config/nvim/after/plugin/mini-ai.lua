95eb072fdfeca3b607c6b63f9a6a61afb5375db7
Stephen Lewis <code@stephenlewis.me>
2025-06-25 18:59:45 +0100

    Remove all things LSP from Neovim
    So sick of things breaking. Call me when they finally ship proper
    out-the-box LSP and completion support.

--- .config/nvim/after/plugin/mini-ai.lua
+++
@@ -1,5 +0,0 @@
--- Configure the mini.ai plugin. Extends and creates `a` and `i` text objects.
---
--- @see https://github.com/echasnovski/mini.nvim
-
-require('mini.ai').setup()
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,18 +6,15 @@ vim.g.maplocalleader = ' '
 -- Configure options.
 require 'monooso.options'
 
+-- Configure providers.
+require 'monooso.providers'
+
 -- Configure common key bindings.
 -- Refer to individual plugin files for plugin-specific key bindings.
 require 'monooso.keys'
 
--- Configure providers.
-require 'monooso.providers'
-
 -- Install and configure plugins using Lazy.
 require 'monooso.lazy'
-
--- Configure and enable language servers.
-require 'monooso.lsp'
 
 -- Configure diagnostic messages.
 require 'monooso.diagnostic'
