136659d48634f7fcfbfece6f0ba718b11de01132
Stephen Lewis <code@stephenlewis.me>
2025-07-31 02:55:52 +0100

    Replace mini.completion with blink-cmp

---
+++ .config/nvim/after/plugin/blink.lua
@@ -0,0 +1,5 @@
+-- Configure the blink.cmp plugin, for great autocomplete success.
+-- @see https://github.com/saghen/blink.cmp
+require('blink.cmp').setup({
+  snippets = { preset = 'mini_snippets' }
+})
---
+++ .config/nvim/lua/monooso/plugins/blink.lua
@@ -0,0 +1,7 @@
+-- Install Blink for autocompletion with Copilot support.
+--
+-- @see https://github.com/saghen/blink.cmp
+return {
+    'saghen/blink.cmp',
+    version = '1.*'
+}
