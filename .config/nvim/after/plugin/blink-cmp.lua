18e8970422d9c03d4176cfb3608e715ae7197de6
Stephen Lewis <code@stephenlewis.me>
2025-07-31 23:00:30 +0100

    Ditch mini.snippets, tweak blink-cmp behaviour

--- .config/nvim/after/plugin/blink-cmp.lua
+++ .config/nvim/after/plugin/blink-cmp.lua
@@ -1,5 +1,5 @@
 -- Configure the blink.cmp plugin, for great autocomplete success.
 -- @see https://github.com/saghen/blink.cmp
 require('blink.cmp').setup({
-  snippets = { preset = 'mini_snippets' }
+  keymap = { preset = 'enter' }
 })
