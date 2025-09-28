9aaf8949aad93b1a4d60c238aeb4297137d4bdcf
Stephen Lewis <code@stephenlewis.me>
2025-09-28 22:28:43 +0100

    Enable additional language servers by default

--- .config/nvim/lua/monooso/lsp.lua
+++ .config/nvim/lua/monooso/lsp.lua
@@ -3,4 +3,11 @@
 -- If a language server cannot be found, Neovim will simply ignore the config.
 -- Running `:checkhealth lsp` will display a warning for each missing language server,
 -- but everything works correctly.
-vim.lsp.enable({ 'elixirls', 'ruby_lsp' })
+
+vim.lsp.enable({
+    'elixirls',
+    'html',
+    'jsonls',
+    'lua_ls',
+    'ruby_lsp'
+})
