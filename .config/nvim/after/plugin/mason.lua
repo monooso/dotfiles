cad746f1260418d32a1257ff2e5f62af9227999b
Stephen Lewis <code@stephenlewis.me>
2025-09-28 22:27:00 +0100

    Install Mason to manage LSPs in Neovim
    Much simpler than managing them separately. The only exception is the
    Ruby LSP, which doesn't play nicely with Mason.

---
+++ .config/nvim/after/plugin/mason.lua
@@ -0,0 +1,5 @@
+-- Configure the Mason plugin.
+--
+-- @see https://github.com/mason-org/mason.nvim
+
+require('mason').setup()
---
+++ .config/nvim/lua/monooso/plugins/mason.lua
@@ -0,0 +1,6 @@
+-- Install Mason, for managing (most) LSPs.
+--
+-- @see https://github.com/mason-org/mason.nvim
+return {
+    'mason-org/mason.nvim'
+}
