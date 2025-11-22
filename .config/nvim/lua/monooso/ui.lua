bc4b0e3de28813685fade2524c4b46ff20a22067
Stephen Lewis <code@stephenlewis.me>
2025-11-22 12:55:11 +0000

    Revert to Tokyo Night, again

--- .config/nvim/lua/monooso/ui.lua
+++ .config/nvim/lua/monooso/ui.lua
@@ -1,10 +1,10 @@
 -- Configures Neovim UI tweaks.
 
--- Use the default colour scheme with a transparent background.
+-- Use a transparent background colour, to mask any minor discrepancies with the terminal.
 vim.api.nvim_create_autocmd('ColorScheme', {
     callback = function()
         vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     end
 })
 
-vim.cmd([[ colorscheme default ]])
+vim.cmd([[ colorscheme tokyonight ]])
