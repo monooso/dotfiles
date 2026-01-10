db19e0bf5400c9ab48b185bbd3945627c6b8633e
Stephen Lewis <code@stephenlewis.me>
2026-01-10 02:07:31 +0000

    Add "move up directories" abbreviation to Fish

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,7 +1,12 @@
-# Simple abbreviations. More complex "shortcuts" are saved as functions.
-
 # Common shell commands
 abbr -a c clear
 
 # Podman > Docker
 abbr -a docker podman
+
+# .... -> cd ../../../
+function cdfinity
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+
+abbr --add dotdot --regex '^\.\.+$' --function cdfinity
