40caad9025a9c2b38f3608fa612fdae3380274c5
Stephen Lewis <code@stephenlewis.me>
2026-01-10 01:54:20 +0000

    Add basic fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+# Initialise Homebrew--this MUST come before any `command` checks
+if test -x /home/linuxbrew/.linuxbrew/bin/brew
+   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+end
+
+# Set up fzf shell integration
+if command -q fzf
+  fzf --fish | source
+end
+
+# Initialise Starship prompt
+if command -q starship
+  starship init fish | source
+end
