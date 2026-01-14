8a62c8094eb629223fa82e29a1ba68569f7634ad
Stephen Lewis <code@stephenlewis.me>
2026-01-14 01:04:43 +0000

    Quickly create or attach to a tmux session using `ts`
    Good job, Fish.

---
+++ .config/fish/functions/ts.fish
@@ -0,0 +1,22 @@
+# Create or attach to a tmux session named
+# after the current directory.
+function ts
+  set session_name (basename $PWD)
+
+  if ! tmux has-session -t $session_name 2> /dev/null
+    tmux new-session -s $session_name -n nvim -d
+    tmux new-window -t $session_name -n llm
+    tmux new-window -t $session_name -n shell
+    tmux new-window -t $session_name -n serve
+
+    # Launch OpenCode in the second window.
+    tmux select-window -t $session_name:2
+    tmux send-keys -t $session_name:2 "opencode" C-m
+
+    # Launch Neovim in the first window.
+    tmux select-window -t $session_name:1
+    tmux send-keys -t $session_name:1 "nvim" C-m
+  end
+
+  tmux attach -t $session_name
+end
