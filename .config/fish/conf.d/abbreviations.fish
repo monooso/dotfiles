9c45a4dbbb565702f0009412e9c2b4ecbb83ae84
Stephen Lewis <code@stephenlewis.me>
2026-02-27 17:07:42 +0000

    feat: reinstate git aliases

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -2,7 +2,20 @@
 abbr -a c clear
 abbr -a t tmux
 
-# Aliases
+# Git (general)
+abbr -a ga "git add"
+abbr -a gc "git commit"
+abbr -a gd "git diff"
+abbr -a gl "git log -10"
+abbr -a gs "git status -sb | sort -bf"
+
+# Git (branches)
+abbr -a gbc "git checkout -b "      # create
+abbr -a gbl "git branch --color"    # list
+abbr -a gbr "git branch --move"     # rename
+abbr -a gbs "git checkout "         # switch
+
+# Alias docker to podman
 abbr -a docker podman
 
 # Custom scripts and binaries
--- .config/fish/functions/gs.fish
+++
@@ -1,3 +0,0 @@
-function gs
-  git status -sb | sort -bf
-end
