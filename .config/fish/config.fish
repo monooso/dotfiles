7f9ebd8507023860ac6cd09fdcef5f04a6c3991c
Stephen Lewis <code@stephenlewis.me>
2026-03-31 13:21:20 +0100

    feat(fish): initialise Zoxide in fish
    Not convinced this is going to stick.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ set -g fish_key_bindings fish_vi_key_bindings
 
 # Initialise Homebrew--this MUST come before any `command` checks
 if test -x /home/linuxbrew/.linuxbrew/bin/brew
-   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
 end
 
 # Add ~/.local/bin to the path
@@ -14,20 +14,25 @@ fish_add_path ~/.local/bin
 
 # Export the RIPGREP_CONFIG_PATH variable if `rg` exists
 if command -q rg
-  set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
+    set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
 end
 
 # Set up fzf shell integration
 if command -q fzf
-  fzf --fish | source
+    fzf --fish | source
 end
 
 # Activate Mise
 if command -q mise
-  mise activate fish | source
+    mise activate fish | source
 end
 
 # Initialise Starship prompt
 if command -q starship
-  starship init fish | source
+    starship init fish | source
+end
+
+# Initialise Zoxide
+if command -q zoxide
+    zoxide init fish | source
 end
