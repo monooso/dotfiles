464153bd59316e5a1fbfeb2f3f9916215e67c7ac
Stephen Lewis <code@stephenlewis.me>
2026-05-13 17:12:33 +0100

    feat(neovim): do not remove colour scheme background
    Trying this out for a while in Konsole.

--- .config/nvim/lua/monooso/ui.lua
+++ .config/nvim/lua/monooso/ui.lua
@@ -1,8 +1,8 @@
 -- Configures Neovim UI tweaks.
 
 -- Use a transparent background colour, to mask any minor discrepancies with the terminal.
-vim.api.nvim_create_autocmd('ColorScheme', {
-    callback = function()
-        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    end
-})
+-- vim.api.nvim_create_autocmd('ColorScheme', {
+--     callback = function()
+--         vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+--     end
+-- })
